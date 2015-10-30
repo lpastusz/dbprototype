6adbf6ae5eead292a300569c5539ddf5733a2201
Lukáš Pastuszek <lukas.pastuszek@gmail.com>
2015-10-30 12:29:19 +0200

    dbtest

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -6,6 +6,18 @@
  */
 
 module.exports = {
-	
-};
 
+	getCourses : function(req, res) {
+
+		console.log('gg');
+
+		Course.find({}).exec(function(err, result) {
+
+			if (err) { res.view(500); return; }
+
+			res.json(result);
+
+		});
+	}
+
+};
